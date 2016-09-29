ab54020648f9c4000c3344101060dd07215b8362
Ignat Mihai <mihai.ignat@assist.ro>
2016-09-29 18:00:27 +0300

    WIP adding splash

---
+++ app/src/main/java/com/example/mihai/realplay/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.mihai.realplay;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.mihai.realplay.Utils.Util;
+
+public class SplashScreen extends Activity {
+
+    private final int SPLASH_SCREEN_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Util.openActivityClosingParent(SplashScreen.this, MainActivity.class);
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+}
